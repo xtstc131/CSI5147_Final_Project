80668892b23e5dbb82ef6bb044d9660441c9efb2
malloxia <mallxia@163.com>
2020-04-01 22:29:21 -0400

    Create first playable demo
    1. Create a ball controlled by Keyboard, Arrowup, down, left, right for moving, Q and E for rotating, Space for stopping the ball.
    2. Attache camera to Ball, and let it move with ball
    3. Import some Textures and Materials for blocks and balls.
    4. Build a demo map for 3D Balance Ball game.

---
+++ Assets/Scripts/BallController.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private Rigidbody rigid;
+
+    public float Force = 5f;
+    private Vector3 dir;
+
+    private bool brake;
+
+    private int spin;
+    public int Torque = 10;
+    void Start()
+    {
+        rigid = this.GetComponent<Rigidbody>();
+    }
+
+
+    // Update is called once per frame
+
+    void Update()
+    {
+        dir = new Vector3(-Input.GetAxis("Horizontal"), 0, -Input.GetAxis("Vertical"));
+        brake = Input.GetKey(KeyCode.Space);
+        if(Input.GetKey(KeyCode.Q))
+        {
+            spin = -1;
+        }
+        else if(Input.GetKey(KeyCode.E))
+        {
+            spin = 1;
+        }
+        else
+        {
+            spin = 0;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        if (!brake)
+        {
+            rigid.AddForce(dir * Force, ForceMode.Force);
+            rigid.AddTorque(Vector3.up * spin * Torque);
+        }
+        else
+        {
+            rigid.velocity = Vector3.Lerp(rigid.velocity, new Vector3(0, rigid.velocity.y, 0), 0.2f);
+        }
+    }
+}
---
+++ Assets/Scripts/Camera.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[ExecuteInEditMode]
+public class Camera : MonoBehaviour
+{
+
+    private Transform trans;
+
+    public Transform Target;
+
+    public Vector3 dis =  new Vector3(0,2,-5);
+    // Start is called before the first frame update
+    void Start()
+    {
+         trans = this.transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        trans.position = Target.position + dis;
+        trans.LookAt(Target);
+    }
+}
